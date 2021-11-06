1a6fff51238509282c52ae0d392f44acc450d625
Daybreak2019 <li.wen@wsu.edu>
2021-11-05 21:25:39 -0700

    trace single PY

--- tool/PyIns/source/main.c
+++ tool/PyIns/source/main.c
@@ -1,17 +1,67 @@
 #include <Python.h>
 #include <frameobject.h>
 
-
-static int lineCounter = 0;
+#define DEFAULT_CODE ("def adder(a, b):\n"\
+                      " return a + b\n"\
+                      "x = 3\n"\
+                      "y = 4\n"\
+                      "print(adder(x, y))\n")
 
 int TraceBack(PyObject *obj, PyFrameObject *frame, int what, PyObject *arg)
 {
-    if(what == PyTrace_LINE)
+    switch(what)
     {
-        lineCounter += 1;
-        printf("line %d\n", lineCounter);
+        case PyTrace_LINE:
+        {
+            printf("PyTrace_LINE\n");
+            break;
+        }
+        case PyTrace_CALL:
+        {
+            printf("PyTrace_CALL\n");
+            break;
+        }
+        case PyTrace_EXCEPTION:
+        {
+            printf("PyTrace_EXCEPTION\n");
+            break;
+        }
+        case PyTrace_RETURN:
+        {
+            printf("PyTrace_RETURN\n");
+            break;
+        }
+        case PyTrace_OPCODE:
+        {
+            printf("PyTrace_OPCODE\n");
+            break;
+        }
+        default:
+        {
+            printf("default: %d\n", what);
+            break;
+        }
     }
+    
     return 0;
+}
+
+char *LoadCode (char *PyFile)
+{
+    assert (PyFile != NULL);
+    FILE *PyF = fopen (PyFile, "r");
+    assert (PyF != NULL);
+
+    fseek(PyF, 0L, SEEK_END);
+    size_t PySize = ftell(PyF) + 4;
+    char *PyCode = (char *)malloc (PySize);
+    assert (PyCode != NULL);
+    memset (PyCode, 0, PySize);
+
+    fseek(PyF, 0L, SEEK_SET);
+    fread (PyCode, 1, PySize-4, PyF);
+    
+    return PyCode;
 }
 
 int main(int argc, char *argv[])
@@ -26,11 +76,8 @@ int main(int argc, char *argv[])
     
     PyEval_SetTrace(TraceBack, NULL);
     
-    char *code = "def adder(a, b):\n"
-                 " return a + b\n"
-                 "x = 3\n"
-                 "y = 4\n"
-                 "print(adder(x, y))\n";
+    //char *code = DEFAULT_CODE;
+    char *code = LoadCode (argv[1]);
     
     PyRun_SimpleString(code);
     Py_Finalize();

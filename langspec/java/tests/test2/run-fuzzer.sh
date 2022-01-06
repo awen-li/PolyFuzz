b2e60a0216c568a1d7926cc571784790937c1fb8
Daybreak2019 <li.wen@wsu.edu>
2022-01-05 23:19:59 -0800

    cross Java-C fuzzing: timeout issue exists

---
+++ langspec/java/tests/test2/run-fuzzer.sh
@@ -0,0 +1,25 @@
+
+export AFL_SKIP_BIN_CHECK=1
+
+if [ ! -d "fuzz" ]; then
+    mkdir -p fuzz/in
+    cp seeds/* fuzz/in/
+    cp test2.jar fuzz/
+    cp /usr/lib/JavaCovPCG/ -rf fuzz/
+fi
+
+cd fuzz
+
+afl-system-config
+
+#pilot fuzzing: max path length
+export AFL_BB_NUM=1024
+
+#enable debug for child process
+export AFL_DEBUG_CHILD=1
+
+#enable crash exit code 
+export AFL_CRASH_EXITCODE=100
+
+afl-fuzz -i in/ -o out -m none -d -- java -jar test2.jar @@
+
--- langspec/java/tests/test2/src/tests/Main.java
+++ langspec/java/tests/test2/src/tests/Main.java
@@ -1,5 +1,11 @@
 package tests;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,13 +14,22 @@ public class Main {
 		
 		if (args.length != 0)
 		{
-			Key = Integer.parseInt(args[0]);
+			Path path = Paths.get(args[0]);
+			try
+			{
+				List<String> lines = Files.readAllLines(path);
+				Key = Integer.parseInt(lines.get(0));
+			}
+			catch (Exception e)
+			{
+				;
+			}
 		}
-		
+
 		PwManage Pm = new PwManage ();
         String pw = Pm.getPwd(Key);
-
-        System.out.println ("pw = " + pw);
+        System.out.println ("Run over -> " + pw);
+        System.exit(0);
 	}
 
 }

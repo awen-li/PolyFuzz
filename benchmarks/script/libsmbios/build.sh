0938eeeeef91bfd91c28455a87ad36ee016d199a
Daybreak2019 <li.wen@wsu.edu>
2022-01-11 17:47:19 -0800

    setup libsmbios

---
+++ benchmarks/script/libsmbios/build.sh
@@ -0,0 +1,36 @@
+
+
+export ROOT=`cd ../../ && pwd`
+export target=libsmbios
+
+function compile ()
+{
+	if [ ! -d "$ROOT/$target" ]; then
+		git clone https://github.com/dell/libsmbios.git
+	fi
+
+	pushd $target
+	
+	export CC="afl-cc"
+	export CXX="afl-c++"
+	
+	./autogen.sh
+	make clean && make && make install
+	
+	# install python
+	if [ -n "$CONDA_PYTHON_EXE" ]; then
+		cp /usr/local/lib/python3.9/site-packages/libsmbios_c -rf /root/anaconda3/lib/python3.9/site-packages/
+		rm -rf /usr/local/lib/python3.9/site-packages/libsmbios_c
+	fi
+	
+	popd
+}
+
+# 1. compile the C unit
+cd $ROOT
+compile
+
+# 2. summarize the Python unit
+PyDir=$target/src/python
+python -m parser $PyDir
+cp $PyDir/py_summary.xml $ROOT/script/$target/

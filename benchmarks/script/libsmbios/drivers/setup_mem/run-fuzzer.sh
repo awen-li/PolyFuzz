d332ead8a0dc10a0a0e162eeaf8e0bef4fc852d9
Daybreak2019 <li.wen@wsu.edu>
2022-01-13 08:15:02 -0800

    libsmbios driver: setup-mem

---
+++ benchmarks/script/libsmbios/drivers/setup_mem/run-fuzzer.sh
@@ -0,0 +1,23 @@
+export AFL_SKIP_BIN_CHECK=1
+
+if [ ! -f "./tests/test-10" ]; then
+	python gen_tests.py
+fi
+
+if [ ! -d "fuzz" ]; then
+   mkdir -p fuzz/in
+   cp ./tests/* fuzz/in/
+fi
+
+cd fuzz
+afl-system-config
+
+#enable debug for child process
+#export AFL_DEBUG_CHILD=1
+
+#enable crash exit code
+export AFL_CRASH_EXITCODE=100
+
+cp ../../py_summary.xml ./
+afl-fuzz $1 $2 -i in/ -o out -m none -d -- python ../setup_mem.py  @@
+
---
+++ benchmarks/script/libsmbios/drivers/setup_mem/setup_mem.py
@@ -0,0 +1,33 @@
+import sys
+import libsmbios_c.memory as Mem
+import pyprob
+
+pyprob.Setup('py_summary.xml', 'setup_mem.py')
+
+pageunit = 4096
+
+def page_num (Tf):
+    Index = Tf.find ('-')
+    pn = int (Tf[Index+1:]) + 1
+    return pn
+
+if __name__ == '__main__':
+    try:
+        Tf = sys.argv[1]
+        pagenum = page_num (Tf)
+        
+        Tf = Tf.encode('utf-8')
+        memObj = Mem.MemoryAccess(Mem.MEMORY_GET_NEW | Mem.MEMORY_UNIT_TEST_MODE, Tf)
+        
+        Offset = 1024
+        for i in range(Offset):
+            memObj.write(chr(ord("a") + i).encode('utf-8'), i)
+
+        for i in range(pagenum):
+            memObj.write(chr(ord("0") + i).encode('utf-8') * pageunit, Offset + (pageunit * i))
+
+        memObj.close_hint(1)
+    except Exception as e:
+        print (e)
+        pyprob.PyExcept (type(e).__name__, __file__, e.__traceback__.tb_lineno)
+

98c3cfc66f20f38b78ccc1dde14b236d2d8d7fdf
Daybreak2019 <li.wen@wsu.edu>
2022-01-13 11:41:31 -0800

    libsmbios driver: leak_mem

---
+++ benchmarks/script/libsmbios/drivers/leak_mem/run-fuzzer.sh
@@ -0,0 +1,19 @@
+export AFL_SKIP_BIN_CHECK=1
+
+if [ ! -d "fuzz" ]; then
+   mkdir -p fuzz/in
+   cp ./tests/* fuzz/in/
+fi
+
+cd fuzz
+afl-system-config
+
+#enable debug for child process
+#export AFL_DEBUG_CHILD=1
+
+#enable crash exit code
+export AFL_CRASH_EXITCODE=100
+
+cp ../../py_summary.xml ./
+afl-fuzz $1 $2 -i in/ -o out -m none -d -- python ../leak_mem.py  @@
+

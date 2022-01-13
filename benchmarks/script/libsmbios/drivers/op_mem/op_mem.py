abea5f171c7bc301ac2ac18c6fa64294aaadfc61
Daybreak2019 <li.wen@wsu.edu>
2022-01-13 12:13:04 -0800

    libsmbios driver: op_mem

--- benchmarks/script/libsmbios/drivers/op_mem/op_mem.py
+++ benchmarks/script/libsmbios/drivers/op_mem/op_mem.py
@@ -1,4 +1,5 @@
 import sys
+import random
 import libsmbios_c.memory as Mem
 import pyprob
 
@@ -25,10 +26,28 @@ if __name__ == '__main__':
 
         for i in range(pagenum):
             memObj.write(chr(ord("0") + i).encode('utf-8') * pageunit, Offset + (pageunit * i))
+            
+        memObj.search("abc".encode("utf-8"), 0, 4096, 1);
+        
+        if pagenum > 2:  
+            index = random.randint (1, pagenum-1)
+            memObj.search("00000000000000000000000000000".encode("utf-8"), 0, pageunit*index, 1);
+        else:
+            index = random.randint (0, pagenum)
+            memObj.search("00000000000000000000000000000".encode("utf-8"), 2048, pageunit*index, 1);
+           
+        if pagenum == 1:
+            offset = random.randint (0, 2048)
+            size   = random.randint (0, 4096)
+            memObj.read(offset, size)
+        else:    
+            offset = random.randint (0, pagenum) * pageunit
+            size = random.randint (0, pagenum) * pageunit
+            memObj.read(offset, size)
 
-        memObj.close_hint(1)
         del(memObj)
         
     except Exception as e:
+        print (e)
         pyprob.PyExcept (type(e).__name__, __file__, e.__traceback__.tb_lineno)
 

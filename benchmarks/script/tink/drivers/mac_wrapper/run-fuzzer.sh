491c941020d974c1a01c93de6b2b07ff4596e262
Daybreak2019 <li.wen@wsu.edu>
2022-01-18 08:56:11 -0800

    tink drivers: mac_wrapper

---
+++ benchmarks/script/tink/drivers/mac_wrapper/run-fuzzer.sh
@@ -0,0 +1,25 @@
+export AFL_SKIP_BIN_CHECK=1
+
+if [ ! -d "fuzz" ]; then
+   mkdir -p fuzz/in
+   cp ./tests/* fuzz/in/
+fi
+
+cd fuzz
+afl-system-config
+
+#enable debug for child process
+#export AFL_DEBUG_CHILD=1
+
+#enable crash exit code
+export AFL_CRASH_EXITCODE=100
+
+export TINK_SRC_PATH=`cd ../../../../ && pwd`/tink
+
+cp ../../py_summary.xml ./
+if [ "$?" != "0" ]; then
+	echo "copy py_summary.xml fail, please check the configuration!!!!"
+	exit 0
+fi
+afl-fuzz $1 $2 -i in/ -o out -m none -d -- python ../mac_wrapper.py  @@
+

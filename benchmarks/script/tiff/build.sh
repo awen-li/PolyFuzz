6f5a5ba209494d8937ea7939bf62908e1910567b
Daybreak2019 <li.wen@wsu.edu>
2022-03-10 08:32:25 -0800

    script done for C-bench:tiff

--- benchmarks/script/tiff/build.sh
+++ benchmarks/script/tiff/build.sh
@@ -9,8 +9,10 @@ cd $BenchDir
 tar -zxvf $target.tar.gz
 
 cd $target
+find ./ -name branch_vars.bv | xargs rm -rf
+find ./ -name cmp_statistic.info | xargs rm -rf
 ./configure --prefix=/root/anaconda3/
 make clean && make CC=afl-cc CXX=afl-cc && make install
 
 cd $ROOT
-ple -s ./seeds -d . -B $BenchDir
+ple -s ./seeds -d . -B $BenchDir/$target

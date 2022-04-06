7c313e9210395665c62e6b5c21d48d314e6e5141
Jinyang Ruan <jinyang.ruan@wsu.edu>
2022-04-06 14:57:02 -0700

    python baseline added

---
+++ baseline/oss-fuzz/projects/pyyaml_fuzzer.sh
@@ -0,0 +1,15 @@
+cd ../
+
+#build docker image
+python infra/helper.py build_image pyyaml
+
+#build fuzz targets
+python infra/helper.py build_fuzzers --sanitizer address pyyaml
+
+cd projects/pyyaml
+
+pip3 install hypothesis
+
+nohup python -u fuzz_loader.py > full.log 2>&1 &
+
+nohup python extract_log.py

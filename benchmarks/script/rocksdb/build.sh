33571847d528f910d860e58ae5e172a193e5f2c3
Daybreak2019 <li.wen@wsu.edu>
2022-05-01 21:16:23 -0700

    build rockdb

---
+++ benchmarks/script/rocksdb/build.sh
@@ -0,0 +1,50 @@
+
+
+export ROOT=`cd ../../ && pwd`
+export target=rocksdb
+
+function instrument_java ()
+{
+	inst_dir=$1
+	jar_name=$2
+	
+	pushd $inst_dir
+	
+	java -cp .:$JavaCovPCG/JavaCovPCG.jar JCovPCG.Main -t ./
+	cp sootOutput/* -rf ./
+	rm -rf sootOutput
+
+	jar -cvfm $jar_name META-INF/MANIFEST.MF *
+	chmod a+x $jar_name
+	cp $jar_name ../
+	mv EXTERNAL_LOC $ROOT/script/$target
+	
+	popd
+}
+
+function compile ()
+{
+	if [ -d "$ROOT/$target" ]; then
+		rm -rf $ROOT/$target
+	fi
+	
+	git clone https://github.com/facebook/rocksdb.git
+
+	pushd $target
+	
+	# compile native
+	cp $ROOT/script/$target/Makefile $ROOT/$target/ -f
+    make -j 4 rocksdbjavastatic
+	
+	# instrument java
+	jar_dir=$ROOT/$target/target/classes
+	#instrument_java $jar_dir "jansi-2.4.1-SNAPSHOT.jar"
+	
+	popd
+}
+
+# 1. compile the C unit
+cd $ROOT
+compile
+
+

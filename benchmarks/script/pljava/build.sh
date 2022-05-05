c9c2dfb277e0ad0ef60dbdd10e7f4c4fb46f3f39
Daybreak2019 <li.wen@wsu.edu>
2022-05-05 11:03:44 -0700

    pljava building script

---
+++ benchmarks/script/pljava/build.sh
@@ -0,0 +1,74 @@
+
+
+export ROOT=`cd ../../ && pwd`
+export target=pljava
+
+function deps ()
+{
+	apt-get install openjdk-11-jdk
+	apt-get install -y postgresql-server-dev-all
+	apt-get install -y libkrb5-dev
+}
+
+function instrument_java ()
+{
+	inst_dir=$1
+	jar_name=$2
+	
+	if [ ! -d "$inst_dir" ]; then
+		mkdir $inst_dir
+		cd $inst_dir
+		jar -xvf ../$jar_name
+		cd --
+	fi
+	
+	pushd $inst_dir
+
+	echo "5000" > INTERAL_LOC
+	java -cp .:$JavaCovPCG/JavaCovPCG.jar JCovPCG.Main -t com/github/luben/zstd
+	cp sootOutput/* -rf com/github/luben/zstd/
+	rm -rf sootOutput
+	
+	mv linux/amd64/branch_vars.bv $ROOT/$target/
+	mv linux/amd64/cmp_statistic.info $ROOT/$target/
+	mv EXTERNAL_LOC $ROOT/script/$target
+	cat branch_vars.bv >> $ROOT/$target/branch_vars.bv
+	rm branch_vars.bv
+	rm INTERAL_LOC
+	
+	cp $ROOT/script/$target/MANIFEST.MF ./META-INF/ -f
+	jar -cvfm $jar_name META-INF/MANIFEST.MF *
+	chmod a+x $jar_name
+	cp $jar_name ../
+	
+	popd
+}
+
+function compile ()
+{
+	if [ -d "$ROOT/$target" ]; then
+		rm -rf $ROOT/$target
+	fi
+	
+	git clone https://github.com/tada/pljava.git
+
+	pushd $target
+	
+	export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 
+	#export CC="afl-cc"
+	#cp $ROOT/script/$target/build.sbt $ROOT/$target/ -f
+	mvn  clean  install
+	
+	jar_dir=$ROOT/$target/target/zstd-jni
+	cd target
+	#instrument_java $jar_dir "zstd-jni-1.5.2-2.jar"
+	cd ..
+	
+	popd
+}
+
+# 1. compile the C unit
+cd $ROOT
+compile
+
+

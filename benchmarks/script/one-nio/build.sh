4520bf26126f8f988ce3096ff03126296e43df2b
Daybreak2019 <li.wen@wsu.edu>
2022-05-06 11:48:27 -0700

    instrumentation done for one-nio

--- benchmarks/script/one-nio/build.sh
+++ benchmarks/script/one-nio/build.sh
@@ -3,6 +3,15 @@
 export ROOT=`cd ../../ && pwd`
 export target=one-nio
 
+function get_onenio_deps ()
+{
+	depslist=`ls $ROOT/$target/lib/*.jar`
+	for dep in ${depslist[@]}
+	do
+		echo $dep >> deps
+	done
+}
+
 function instrument_java ()
 {
 	inst_dir=$1
@@ -10,31 +19,27 @@ function instrument_java ()
 	
 	if [ ! -d "$inst_dir" ]; then
 		mkdir $inst_dir
-		cd $inst_dir
-		jar -xvf ../$jar_name
-		cd --
 	fi
 	
-	pushd $inst_dir
+	cd $inst_dir
+	jar -xvf ../$jar_name
 
 	echo "5000" > INTERAL_LOC
-	java -cp .:$JavaCovPCG/JavaCovPCG.jar JCovPCG.Main -t com/github/luben/zstd
-	cp sootOutput/* -rf com/github/luben/zstd/
+	get_onenio_deps
+	java -cp .:$JavaCovPCG/JavaCovPCG.jar JCovPCG.Main -d deps -t one/nio
+	cp sootOutput/* -rf one/nio/
 	rm -rf sootOutput
 	
-	mv linux/amd64/branch_vars.bv $ROOT/$target/
-	mv linux/amd64/cmp_statistic.info $ROOT/$target/
 	mv EXTERNAL_LOC $ROOT/script/$target
 	cat branch_vars.bv >> $ROOT/$target/branch_vars.bv
 	rm branch_vars.bv
 	rm INTERAL_LOC
+	rm deps
 	
 	cp $ROOT/script/$target/MANIFEST.MF ./META-INF/ -f
 	jar -cvfm $jar_name META-INF/MANIFEST.MF *
 	chmod a+x $jar_name
 	cp $jar_name ../
-	
-	popd
 }
 
 function compile ()
@@ -50,9 +55,9 @@ function compile ()
 	cp $ROOT/script/$target/ant-build.xml $ROOT/$target/build.xml -f
 	ant
 	
-	jar_dir=$ROOT/$target/target/one-nio-instm
-	cd target
-	#instrument_java $jar_dir "zstd-jni-1.5.2-2.jar"
+	jar_dir=$ROOT/$target/build/one-nio-instm
+	cd build
+	instrument_java $jar_dir "one-nio.jar"
 	cd ..
 	
 	popd

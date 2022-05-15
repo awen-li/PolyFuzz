66aadbdf99d0f810e03d5b7757ac75e4136033a4
Daybreak2019 <li.wen@wsu.edu>
2022-05-14 19:06:34 -0700

    bottleneck driver - random-shape2

---
+++ benchmarks/script/bottleneck/drivers/random_shape2/random_shape.py
@@ -0,0 +1,77 @@
+import sys
+import numpy as np
+import bottleneck as bn
+import pyprob
+
+pyprob.Setup('py_summary.xml', 'random_shape.py')
+
+
+def WriteTest (FileName):
+    Bytes=b"\x06\x0112a7647548545486868564509686676"
+    with open(FileName, 'wb') as Fw:
+        Fw.write (Bytes)
+
+
+def LoadTest (FileName):
+    with open(FileName, 'rb') as Fw:
+        Value = Fw.read ()
+        return Value
+
+
+if __name__ == '__main__':
+    try:
+        #WriteTest ("tests/test")
+        
+        Value  = bytearray (LoadTest (sys.argv[1]))
+        Length = len (Value)
+        if Length < 4:
+            exit (0)
+            
+        #print (Length)
+        
+        # 0b: dimenson
+        d = Value[0]%8
+        if d < 3:
+            exit (0)
+        if d > Length-1:
+            d = Length-1
+        print (d)
+         
+        # 0-Length-1: shape value   
+        Shape = []
+        for i in range(1, d+1):
+            Sval = Value[i]%32
+            if Sval == 0:
+                Sval = 1
+            Shape.append (Sval)
+        
+        print (Shape)
+        
+        Ary = np.random.random(tuple (Shape))
+        #bn.nanmax(Ary)
+        #bn.nanmin(Ary)
+        #bn.nanmedian(Ary)
+        #bn.nanargmin(Ary)
+        #bn.nanargmax(Ary)
+        
+        R_Shape = []
+        R_Shape.append (0)
+        R_Shape.append (d-1)
+        for i in range (1, d-1):
+            R_Shape.append (i)
+            
+        print (R_Shape)
+        
+        R_Ary = Ary.transpose(tuple(R_Shape))
+        bn.nanmean(R_Ary)
+        bn.nanstd(R_Ary)
+        bn.nanrankdata(R_Ary)
+        bn.nanargmin(R_Ary)
+        bn.nanargmax(R_Ary)
+        bn.nanmedian(R_Ary)
+        bn.median(R_Ary)
+        bn.ss(R_Ary)
+ 
+    except Exception as e:
+        print (e)
+        pyprob.PyExcept (type(e).__name__, __file__, e.__traceback__.tb_lineno)

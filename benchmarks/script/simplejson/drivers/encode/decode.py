3ccc80d4bfd727e1417ad950308245426eeffcf1
Daybreak2019 <li.wen@wsu.edu>
2022-05-16 13:39:46 -0700

    driver for simplejson

---
+++ benchmarks/script/simplejson/drivers/encode/decode.py
@@ -0,0 +1,28 @@
+import sys
+import pyprob
+import simplejson as json
+
+
+pyprob.Setup('py_summary.xml', 'decode.py')
+
+def LoadInput (TxtFile):
+    Content = ""
+    with open(TxtFile, 'r', encoding='utf-8') as f:
+        Content = f.readlines()
+    return Content
+
+if __name__ == '__main__':
+    try:
+        InputData = str(LoadInput (sys.argv[1]))
+        
+        decoder = json.JSONDecoder()
+        encoder = json.JSONEncoderForHTML()
+        
+        en = encoder.encode (InputData)
+        decoder.decode (en)
+        
+    except Exception as e:
+        print ("Exception")
+        print (e)
+        pyprob.PyExcept (type(e).__name__, __file__, e.__traceback__.tb_lineno)
+    

091c4589a77e12072e9961a226fecbddad05a237
Guangbei <guangbei.yi@wsu.edu>
2022-05-17 23:02:04 -0700

    Update civetweb

--- baseline/oss-fuzz/projects/honggfuzz_bench/civetweb.sh
+++ baseline/oss-fuzz/projects/honggfuzz_bench/civetweb.sh
@@ -2,12 +2,12 @@
 
 sh install_honggfuzz.sh
 
-cd bluez/
+cd civetweb/
 
 mkdir IN
 
 echo A > IN/1
 
-nohup honggfuzz -i IN/ -- fuzz_sdp ___FILE___ > full.log 2>&1 &
+nohup honggfuzz -i IN/ -- civetweb_fuzz1 ___FILE___ > full.log 2>&1 &
 
 python extract_log.py

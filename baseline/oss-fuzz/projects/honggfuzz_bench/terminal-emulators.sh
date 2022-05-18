9b679f8b1941704b91c6442fd4b4f8a2c3e3f1a5
Guangbei <guangbei.yi@wsu.edu>
2022-05-17 23:39:19 -0700

    update script

--- baseline/oss-fuzz/projects/honggfuzz_bench/terminal-emulators.sh
+++ baseline/oss-fuzz/projects/honggfuzz_bench/terminal-emulators.sh
@@ -6,12 +6,12 @@ git clone https://github.com/google/honggfuzz.git
 cd honggfuzz
 make
 make install
-cd..
+cd ..
 rm -rf honggfuzz
 
 cd terminal-emulators
 
-nohup honggfuzz -z -P -i IN/ -E LD_PRELOAD=libclose.so -- xterm -e terminal-test
+nohup honggfuzz -z -P -i IN/ -E LD_PRELOAD=libclose.so -- xterm -e terminal-test > full.log 2>&1 &
 
 python extract_log.py
 

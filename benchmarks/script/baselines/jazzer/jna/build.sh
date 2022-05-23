cdfb3cf4093483e76767fdfa8e43b1f94a07ef3e
Daybreak2019 <li.wen@wsu.edu>
2022-05-22 17:22:34 -0700

    jazzer-bench-jna drivres coding done!

--- benchmarks/script/baselines/jazzer/jna/build.sh
+++ benchmarks/script/baselines/jazzer/jna/build.sh
@@ -1,33 +1,7 @@
 
 
-export ROOT=`cd ../../ && pwd`
+export ROOT=`cd ../../../../ && pwd`
 export target=jna
-
-function instrument_java ()
-{
-	inst_dir=$1
-	jar_name=$2
-	
-	if [ ! -d "$inst_dir" ]; then
-		mkdir $inst_dir
-		cd $inst_dir
-		jar -xvf ../$jar_name
-		cd -
-	fi
-	
-	cd $inst_dir
-	
-	cp $ROOT/$target/INTERAL_LOC $inst_dir/
-
-	java -cp .:$JavaCovPCG/JavaCovPCG.jar JCovPCG.Main -t .
-	cp sootOutput/* -rf .
-	rm -rf sootOutput
-
-	jar -cvfm $jar_name META-INF/MANIFEST.MF *
-	chmod a+x $jar_name
-	cp $jar_name ../
-	mv EXTERNAL_LOC $ROOT/script/$target
-}
 
 function compile ()
 {
@@ -36,7 +10,6 @@ function compile ()
 	fi
 	
 	git clone https://github.com/java-native-access/jna.git
-	
 	
 	JAVA_8=`echo $JAVA_HOME | grep java-8`
 	if [ ! -n "$JAVA_8" ]; then
@@ -47,21 +20,15 @@ function compile ()
 	cd $target
 	
 	# compile native
-	cp $ROOT/script/$target/Makefile $ROOT/$target/native/ -f
 	cd native
 	ant
 	cd ..
     
 	# compile java
 	ant
-	
-	# instrument java
-	jar_dir=$ROOT/$target/build/$target-instm
-	cd build
-	instrument_java $jar_dir "jna.jar" 
 }
 
-# 1. compile the C unit
+
 cd $ROOT
 compile
 
--- benchmarks/script/baselines/jazzer/jna/drivers/AnalyzeELF/src/AnalyzeELF/AnalyzeELF.java
+++ benchmarks/script/baselines/jazzer/jna/drivers/AnalyzeELF/src/AnalyzeELF/AnalyzeELF.java
@@ -3,16 +3,35 @@ package AnalyzeELF;
 
 import java.io.IOException;
 import com.sun.jna.ELFAnalyser;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class AnalyzeELF 
 {
-    public static void main(String[] args)
+    public static void WritOneCase (String Path, byte[] bytes)
     {
-        String InFile = args [0];
+        try
+        {
+            Path p = Paths.get(Path);
+            Files.write(p, bytes);
+        }
+        catch (IOException e) 
+        {
+            return;
+        }
+    }
+
+    public static void fuzzerTestOneInput(byte[] input)
+    {
+        String seed = "seed";
+        
+        WritOneCase (seed, input);
 
         try
         {
-            ELFAnalyser.analyse(InFile);
+            ELFAnalyser.analyse(seed);
         }
         catch (IOException e) 
         {

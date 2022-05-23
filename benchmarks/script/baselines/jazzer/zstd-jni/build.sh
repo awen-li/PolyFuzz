97092eaeb5884c935f52d9383c673fed20b8182c
Daybreak2019 <li.wen@wsu.edu>
2022-05-22 22:32:04 -0700

    debug zstd on jazzer

--- benchmarks/script/baselines/jazzer/zstd-jni/build.sh
+++ benchmarks/script/baselines/jazzer/zstd-jni/build.sh
@@ -3,6 +3,28 @@
 export ROOT=`cd ../../../../ && pwd`
 export target=zstd-jni
 
+function instrument_java ()
+{
+	inst_dir=$1
+	jar_name=$2
+	
+	if [ ! -d "$inst_dir" ]; then
+		mkdir $inst_dir
+		cd $inst_dir
+		jar -xvf ../$jar_name
+		cd --
+	fi
+	
+	pushd $inst_dir
+	
+	cp $ROOT/script/baselines/jazzer/$target/MANIFEST.MF ./META-INF/ -f
+	jar -cvfm $jar_name META-INF/MANIFEST.MF *
+	chmod a+x $jar_name
+	cp $jar_name ../
+	
+	popd
+}
+
 function compile ()
 {
 	if [ -d "$ROOT/$target" ]; then
@@ -15,8 +37,16 @@ function compile ()
 	
 	export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 	update-java-alternatives --set java-1.8.0-openjdk-amd64
-	
+
+	cp $ROOT/script/baselines/jazzer/$target/build.sbt $ROOT/$target/ -f
 	./sbt compile package
+	
+	jar_dir=$ROOT/$target/target/zstd-jni
+	cd target
+	jar_name=`find ./ -name "*.jar"`
+	jar_name=`basename $jar_name`
+	instrument_java $jar_dir $jar_name
+	cd ..
 	
 	popd
 }
@@ -24,5 +54,7 @@ function compile ()
 
 cd $ROOT
 compile
+
+
 
 

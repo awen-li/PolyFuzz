831016e3b8fdaa3a2237ed70d7913f37b663b6c8
Daybreak2019 <li.wen@wsu.edu>
2022-05-27 12:19:54 -0700

    rebuild simplejson

--- benchmarks/script/multi-benches/simplejson/build.sh
+++ benchmarks/script/multi-benches/simplejson/build.sh
@@ -1,7 +1,24 @@
 
 
-export ROOT=`cd ../../ && pwd`
+export ROOT=`cd ../../../ && pwd`
 export target=simplejson
+export ROOT_SCRIPT=$ROOT/script/multi-benches/$target
+
+function collect_branchs ()
+{
+	ALL_BRANCHS=`find $ROOT/$target -name branch_vars.bv`
+	
+	if [ -f "$ROOT_SCRIPT/drivers/branch_vars.bv" ]; then
+		rm $ROOT_SCRIPT/drivers/branch_vars.bv
+	fi
+	
+	echo "@@@@@@@@@ ALL_BRANCHES -----> $ALL_BRANCHS"
+	for branch in $ALL_BRANCHS
+	do
+		cat $branch >> $ROOT_SCRIPT/drivers/branch_vars.bv
+		rm $branch
+	done
+}
 
 function compile ()
 {
@@ -26,6 +43,9 @@ cd $ROOT
 compile
 
 # 2. summarize the Python unit
-PyDir=$target/simplejson
+cd $ROOT/$target/
+PyDir=simplejson
 python -m parser $PyDir
-cp $PyDir/py_summary.xml $ROOT/script/$target/
+cp $PyDir/py_summary.xml $ROOT_SCRIPT/
+
+collect_branchs

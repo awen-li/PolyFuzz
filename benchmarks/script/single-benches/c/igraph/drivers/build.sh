0f029fa6d3bf09162051a9e8a44b5b2b3bb03c90
Daybreak2019 <li.wen@wsu.edu>
2022-05-26 21:53:16 -0700

    polyfuzz - drivers-igraph done !

---
+++ benchmarks/script/single-benches/c/igraph/drivers/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash  
+  
+Root=`pwd`
+
+ALL_TESTS=`ls` 
+for JT in $ALL_TESTS
+do
+	if [ ! -d "$JT" ]; then
+		continue
+    fi
+    
+    if [ ! -d "$JT/tests" ]; then
+    	mkdir $JT/tests
+    fi    
+    cd $JT/tests && tar -xzf ../seed_corpus.tar.gz  && cd -
+
+    cp $FUZZ_HOME/lib/*   $JT/ -rf
+    cp $FUZZ_HOME/include $JT/ -rf
+
+    cd $JT && make clean && make && cd -
+    cp branch_vars.bv $JT/
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$Root/$JT/lib
+
+done
+  
+cd $Root
--- benchmarks/script/single-benches/java/apache-commons/drivers/zip/run-fuzzer.sh
+++ benchmarks/script/single-benches/java/apache-commons/drivers/zip/run-fuzzer.sh
@@ -14,7 +14,7 @@ cd fuzz
 
 #enable debug for child process
 export AFL_DEBUG_CHILD=1
-export TARGET_APP=../../../commons-compress.jar
+export TARGET_APP=$BENCH/script/single-benches/java/apache-commons/commons-compress.jar
 
 #enable crash exit code
 export AFL_CRASH_EXITCODE=100

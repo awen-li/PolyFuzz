7de933352e99c72aee4f041e3cc60e4e2d5cf826
Daybreak2019 <li.wen@wsu.edu>
2022-05-27 19:36:01 -0700

    data collection for containers

--- tool/gather.sh
+++ tool/gather.sh
@@ -28,12 +28,14 @@ do
 		exit 0
 	fi
 	
+	echo -e "@@@ Trying to collect data in $container "
 	for path in $target_path
 	do
 		benches=`docker exec $container ls $path`
 		
 		for bench in $benches
 		do
+			echo -e "\t@@@ Trying to collect data in $container for $bench "
 			file=`docker exec $container find $path/$bench -name perf_periodic.txt`
 			if [ "$file" == "" ]; then
 				continue

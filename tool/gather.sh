55c744e4ba75d881d6626231aaff50becddae250
Daybreak2019 <li.wen@wsu.edu>
2022-05-27 19:28:04 -0700

    data collection for containers

---
+++ tool/gather.sh
@@ -0,0 +1,46 @@
+
+#bench type
+BENCH_TYPE=$1
+if [ ! -n "$BENCH_TYPE" ]; then
+	echo "please input the bench type for data collection[pls - polyfuzz single benches | plm - polyfuzz multi benches]...."
+	exit 0
+fi
+
+#data dir
+DATA_DIR=~/container_data
+if [ ! -d "$DATA_DIR" ]; then
+	mkdir $DATA_DIR
+fi
+
+ALL_CONTAINERS=`docker container ls --format 'table {{.Names}}'`
+for container in $ALL_CONTAINERS
+do
+	if [ "$container" == "NAMES" ]; then
+		continue;
+	fi
+	
+	if [ "$BENCH_TYPE" == "pls" ]; then
+		target_path="/root/xFuzz/benchmarks/script/single-benches/c /root/xFuzz/benchmarks/script/single-benches/python /root/xFuzz/benchmarks/script/single-benches/java"
+	elif [ "$BENCH_TYPE" == "plm" ]; then
+	    target_path="/root/xFuzz/benchmarks/script/multi-benches/"
+	else
+		echo "Unsupport bench type: $BENCH_TYPE"
+		exit 0
+	fi
+	
+	for path in $target_path
+	do
+		benches=`docker exec $container ls $path`
+		
+		for bench in $benches
+		do
+			file=`docker exec $container find $path/$bench -name perf_periodic.txt`
+			if [ "$file" == "" ]; then
+				continue
+			fi
+			
+			log_path=$DATA_DIR/$container"_"$bench"_perf_periodic.txt"
+			docker exec $container cat $file > $log_path 
+		done
+	done
+done

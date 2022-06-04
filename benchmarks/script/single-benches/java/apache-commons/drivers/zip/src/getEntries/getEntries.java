aa304de0fda01f8885063f1427ecf7f8a04c1d28
Daybreak2019 <li.wen@wsu.edu>
2022-06-04 10:04:36 -0700

    update driver

--- benchmarks/script/single-benches/java/apache-commons/drivers/zip/src/getEntries/getEntries.java
+++ benchmarks/script/single-benches/java/apache-commons/drivers/zip/src/getEntries/getEntries.java
@@ -2,13 +2,38 @@ package zip;
 
 import org.apache.commons.compress.archivers.zip.ZipFile;
 import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.utils.IOUtils;
+import java.io.OutputStream;
+
 import java.io.IOException;
 import java.io.File;
 import java.io.InputStream;
 import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.FileNotFoundException;
+
+
 
 public class getEntries 
 {
+    public static final ArchiveStreamFactory factory = ArchiveStreamFactory.DEFAULT;
+    public static File archive;
+    
+    public static void addArchiveEntry(final ArchiveOutputStream out, 
+                                              final String filename, 
+                                              final File infile) throws IOException, FileNotFoundException 
+    {
+        final ArchiveEntry entry = out.createArchiveEntry(infile, filename);
+        out.putArchiveEntry(entry);
+        IOUtils.copy(infile, out);
+        out.closeArchiveEntry();
+    }
+            
     public static void main(String[] args)
     {
         String InFile = args [0];
@@ -24,9 +49,16 @@ public class getEntries
             insputStream.read(bytes);
             insputStream.close();
 
+            String archivename = "archivename.tt";
+            archive = File.createTempFile("test", "." + archivename);
+            archive.deleteOnExit();
+            OutputStream stream = Files.newOutputStream(archive.toPath());
+            ArchiveOutputStream out = factory.createArchiveOutputStream(archivename, stream);
+            addArchiveEntry (out, "in/0", new File (InFile));
+            
             new ZipFile(new SeekableInMemoryByteChannel(bytes)).close();       
         }
-        catch (IOException e) 
+        catch (Exception  e) 
         {
             return;
         }

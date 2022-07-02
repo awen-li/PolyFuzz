a698933b1933c965d5c5d5bbd90b43fc5de7075f
Guangbei <guangbei.yi@wsu.edu>
2022-07-01 23:39:30 -0700

    update test driver

---
+++ benchmarks/script/multi-benches/jep/drivers/JepDr_Test/JepDriver.java
@@ -0,0 +1,39 @@
+package JepDr;
+
+import jep.Jep;
+import jep.JepConfig;
+import jep.Interpreter;
+import jep.SubInterpreter;
+
+public class JepDriver {
+
+    public void run(String argv[]) 
+    {
+        try 
+        {
+            String spt = argv [0];
+
+            JepConfig config = new JepConfig();
+            config.addIncludePaths("subprocess");
+            
+            Interpreter interp = new SubInterpreter(config);
+            interp.runScript(spt);
+
+            //test set
+            interp.set(argv [1], argv [2]);
+
+        }
+        catch (Exception e) 
+        {
+            return;
+        }
+
+    }
+
+    public static void main(String argv[]) throws Throwable 
+    {
+        jep_drive = new JepDriver();
+        jep_drive.run(argv);
+    }
+
+}

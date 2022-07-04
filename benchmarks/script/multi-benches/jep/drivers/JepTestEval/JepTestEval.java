216b6d80039da1f5d84668c468cf3b14d872ea33
Guangbei <guangbei.yi@wsu.edu>
2022-07-03 22:52:52 -0700

    add drivers

---
+++ benchmarks/script/multi-benches/jep/drivers/JepTestEval/JepTestEval.java
@@ -0,0 +1,39 @@
+package JepDr;
+
+import jep.Jep;
+import jep.JepConfig;
+import jep.Interpreter;
+import jep.SubInterpreter;
+
+public class JepTestEval {
+    
+    public void run(String argv[]) 
+    {
+        try 
+        {
+            String spt = argv [0];
+
+            JepConfig config = new JepConfig();
+            config.addIncludePaths("subprocess");
+            
+            Interpreter interp = new SubInterpreter(config);
+            interp.runScript(spt);
+
+            //test eval
+            interp.eval(argv [1]);
+
+        }
+        catch (Exception e) 
+        {
+            return;
+        }
+
+    }
+
+    public static void main(String argv[]) throws Throwable 
+    {
+        jep_drive = new JepDriver();
+        jep_drive.run(argv);
+    }
+
+}
---
+++ benchmarks/script/multi-benches/jep/drivers/JepTestGetValue/JepTestGetValue.java
@@ -0,0 +1,41 @@
+package JepDr;
+
+import jep.Jep;
+import jep.JepConfig;
+import jep.Interpreter;
+import jep.SubInterpreter;
+
+
+public class JepTestGetValue {
+
+    public void run(String argv[]) 
+    {
+        try 
+        {
+            String spt = argv [0];
+
+            JepConfig config = new JepConfig();
+            config.addIncludePaths("subprocess");
+            
+            Interpreter interp = new SubInterpreter(config);
+            interp.runScript(spt);
+
+            interp.set("test",argv [1]);
+            interp.getValue("test");
+
+        }
+        catch (Exception e) 
+        {
+            return;
+        }
+
+    }
+
+    public static void main(String argv[]) throws Throwable 
+    {
+        jep_drive = new JepDriver();
+        jep_drive.run(argv);
+    }
+
+
+}

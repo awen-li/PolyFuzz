8f733a8fd8f35bb2d933e5b0895c17910f3d9403
Daybreak2019 <li.wen@wsu.edu>
2022-09-07 12:02:41 -0700

    atheirs-native: done with aubio

--- benchmarks/script/baselines/atheris/aubio/build.sh
+++ benchmarks/script/baselines/atheris/aubio/build.sh
@@ -13,6 +13,12 @@ function compile ()
 	
 	pushd $target
 	
+	LD_CLANG="$(python -c "import sysconfig; print(sysconfig.get_config_var('LDSHARED'))")"	
+	LD_CLANG=`echo $LD_CLANG | sed 's/^gcc/clang/'`
+	
+	export LDSHARED=$LD_CLANG
+	export CC="clang" CFLAGS="-fsanitize=fuzzer-no-link" CXX="clang++" CXXFLAGS="-fsanitize=fuzzer-no-link"
+
 	pip install .
 
 	popd
--- benchmarks/script/baselines/atheris/aubio/drivers/filter/run-fuzzer.sh
+++ benchmarks/script/baselines/atheris/aubio/drivers/filter/run-fuzzer.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export LD_PRELOAD="$(python -c "import atheris; print(atheris.path())")/asan_with_fuzzer.so"
+export ASAN_OPTIONS=detect_leaks=0
+
 python $BENCH/script/popen_log.py "python filter.py ../test_aubios" &
 
 

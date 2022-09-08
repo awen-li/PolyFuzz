7f301ea4818f02bf1078937cbcb866b44afd5d01
Daybreak2019 <li.wen@wsu.edu>
2022-09-08 12:43:04 -0700

    atheirs-native: done with pillow

--- benchmarks/script/baselines/atheris/Pillow/drivers/fig_process/run-fuzzer.sh
+++ benchmarks/script/baselines/atheris/Pillow/drivers/fig_process/run-fuzzer.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export LD_PRELOAD="$(python -c "import atheris; print(atheris.path())")/asan_with_fuzzer.so"
+export ASAN_OPTIONS=detect_leaks=0
+
 python $BENCH/script/popen_log.py "python fig_process.py ./tests -rss_limit_mb=4096" &
 
 
--- benchmarks/script/baselines/atheris/Pillow/drivers/image_open/run-fuzzer.sh
+++ benchmarks/script/baselines/atheris/Pillow/drivers/image_open/run-fuzzer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export LD_PRELOAD="$(python -c "import atheris; print(atheris.path())")/asan_with_fuzzer.so"
+export ASAN_OPTIONS=detect_leaks=0
 
 python $BENCH/script/popen_log.py "python image_open.py ./tests -rss_limit_mb=4096" &
 

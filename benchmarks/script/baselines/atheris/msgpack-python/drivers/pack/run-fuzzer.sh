e14dc08c22b6cad436a1021822060a03b550b6f3
Daybreak2019 <li.wen@wsu.edu>
2022-09-08 00:03:48 -0700

    atheirs-native: done with msgpack

--- benchmarks/script/baselines/atheris/msgpack-python/drivers/pack/run-fuzzer.sh
+++ benchmarks/script/baselines/atheris/msgpack-python/drivers/pack/run-fuzzer.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+export LD_PRELOAD="$(python -c "import atheris; print(atheris.path())")/asan_with_fuzzer.so"
+export ASAN_OPTIONS=detect_leaks=0
+
 python $BENCH/script/popen_log.py "python pack.py ./tests -rss_limit_mb=4096" &
 

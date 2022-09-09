b5c3ee090b873f65b19206f054059ea932f9fa3a
Daybreak2019 <li.wen@wsu.edu>
2022-09-09 11:50:39 -0700

    atheirs-native: done with pycurl

--- benchmarks/script/baselines/atheris/pycurl/build.sh
+++ benchmarks/script/baselines/atheris/pycurl/build.sh
@@ -12,6 +12,12 @@ function compile ()
 	git clone https://github.com/pycurl/pycurl
 
 	pushd $target
+	
+	LD_CLANG="$(python -c "import sysconfig; print(sysconfig.get_config_var('LDSHARED'))")"	
+	LD_CLANG=`echo $LD_CLANG | sed 's/^gcc/clang/'`
+	
+	export LDSHARED=$LD_CLANG
+	export CC="clang" CFLAGS="-fsanitize=fuzzer-no-link" CXX="clang++" CXXFLAGS="-fsanitize=fuzzer-no-link"
 
 	python setup.py install
 	

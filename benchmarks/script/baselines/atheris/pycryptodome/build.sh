625db619c8fa73df9f74500c8b6c9446260049f8
Daybreak2019 <li.wen@wsu.edu>
2022-09-09 10:15:40 -0700

    atheirs-native: done with pycryptodome

--- benchmarks/script/baselines/atheris/pycryptodome/build.sh
+++ benchmarks/script/baselines/atheris/pycryptodome/build.sh
@@ -12,6 +12,13 @@ function compile ()
 	git clone https://github.com/Legrandin/pycryptodome
 
 	pushd $target
+	
+	LD_CLANG="$(python -c "import sysconfig; print(sysconfig.get_config_var('LDSHARED'))")"	
+	LD_CLANG=`echo $LD_CLANG | sed 's/^gcc/clang/'`
+	
+	export LDSHARED=$LD_CLANG
+	export CC="clang" CFLAGS="-fsanitize=fuzzer-no-link" CXX="clang++" CXXFLAGS="-fsanitize=fuzzer-no-link"
+	export PYCRYPTODOME_DISABLE_DEEPBIND=1
 
 	python setup.py install
 	
--- benchmarks/script/baselines/atheris/pycryptodome/drivers/encrypt-aes/run-fuzzer.sh
+++ benchmarks/script/baselines/atheris/pycryptodome/drivers/encrypt-aes/run-fuzzer.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+export LD_PRELOAD="$(python -c "import atheris; print(atheris.path())")/asan_with_fuzzer.so"
+export ASAN_OPTIONS=detect_leaks=0
+export PYCRYPTODOME_DISABLE_DEEPBIND=1
+
 python $BENCH/script/popen_log.py "python encrypt-aes.py ./tests -rss_limit_mb=4096" &
 

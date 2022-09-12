6128af9643d863aaa689f4dc706f62a0a6931875
Daybreak2019 <li.wen@wsu.edu>
2022-09-11 20:20:25 -0700

    atheirs-native: done with tink

--- benchmarks/script/baselines/atheris/tink/build.sh
+++ benchmarks/script/baselines/atheris/tink/build.sh
@@ -20,6 +20,12 @@ function compile ()
 
 	pushd $target/python
 	
+	LD_CLANG="$(python -c "import sysconfig; print(sysconfig.get_config_var('LDSHARED'))")"	
+	LD_CLANG=`echo $LD_CLANG | sed 's/^gcc/clang/'`
+	
+	export LDSHARED=$LD_CLANG
+	export CC="clang" CFLAGS="-fsanitize=fuzzer-no-link" CXX="clang++" CXXFLAGS="-fsanitize=fuzzer-no-link"
+	
 	cp $ROOT/script/$target/setup.py ./
 	pip3 install .
 

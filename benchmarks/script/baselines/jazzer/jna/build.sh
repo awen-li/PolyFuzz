2a70c7d72bb5fd62fca42737dfc40c4b4222f4c6
Daybreak2019 <li.wen@wsu.edu>
2022-09-11 21:00:07 -0700

    jazzer-native: done with jna

--- benchmarks/script/baselines/jazzer/jna/build.sh
+++ benchmarks/script/baselines/jazzer/jna/build.sh
@@ -19,6 +19,12 @@ function compile ()
 
 	cd $target
 	
+	LD_CLANG="$(python -c "import sysconfig; print(sysconfig.get_config_var('LDSHARED'))")"	
+	LD_CLANG=`echo $LD_CLANG | sed 's/^gcc/clang/'`
+		
+	export LDSHARED=$LD_CLANG
+	export CC="clang" CFLAGS="-fsanitize=fuzzer-no-link" CXX="clang++" CXXFLAGS="-fsanitize=fuzzer-no-link"
+	
 	# compile native
 	cd native
 	ant

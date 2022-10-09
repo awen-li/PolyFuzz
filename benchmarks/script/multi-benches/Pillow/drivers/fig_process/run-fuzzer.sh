eb0c358b9578c41f4f957e42168fa1666c3f5c86
Daybreak2019 <li.wen@wsu.edu>
2022-10-08 21:23:17 -0700

    delete useless tests

--- benchmarks/script/multi-benches/Pillow/drivers/fig_process/run-fuzzer.sh
+++ benchmarks/script/multi-benches/Pillow/drivers/fig_process/run-fuzzer.sh
@@ -14,12 +14,12 @@ cd fuzz
 #enable crash exit code
 export AFL_CRASH_EXITCODE=100
 
-cp ../../../py_summary.xml ./
+cp ../../py_summary.xml ./
 if [ "$?" != "0" ]; then
 	echo "copy py_summary.xml fail, please check the configuration!!!!"
 	exit 0
 fi
 
-export AFL_PL_HAVOC_NUM=512
+export AFL_PL_HAVOC_NUM=128
 afl-fuzz $1 $2 -i in/ -o out -m none -d -- python ../fig_process.py  @@
 
